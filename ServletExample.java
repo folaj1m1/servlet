92d5147e324d0271656f9d5d7042c2b7af1d8d51
folaj1m1 <fola.elug@gmail.com>
2015-06-01 23:35:53 +0100

    Another update
    Just testing

--- ServletExample.java
+++ ServletExample.java
@@ -17,7 +17,7 @@ public class ServletExample extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("Hello Java good morning!");
+		out.println("Hello Java good morning!!");
 	}
 
 }
