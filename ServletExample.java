2c57acd5621a9efb285f38794217132fbd70fc42
folaj1m1 <fola.elug@gmail.com>
2015-06-01 23:26:25 +0100

    Update servlet Example

--- ServletExample.java
+++ ServletExample.java
@@ -17,7 +17,7 @@ public class ServletExample extends HttpServlet {
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("Hello Java!");
+		out.println("Hello Java good morning!");
 	}
 
 }
